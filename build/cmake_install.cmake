b63d089c84feb1a69be1ca291e0de4e15753a121
Cléa //purplelines 🏳️‍⚧️ <wawlebazar@gmail.com>
2025-11-27 09:14:42 +0100

    Update cmake_install.cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/svergolini/Documents/GitHub/hyprland-glass-shader/Hypr-GlassWindow/build/install_local_manifest.txt"
+  file(WRITE "/home/purplelines/Documents/GitHub/hyprland-glass-shader/Hypr-GlassWindow/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/svergolini/Documents/GitHub/hyprland-glass-shader/Hypr-GlassWindow/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/purplelines/Documents/GitHub/hyprland-glass-shader/Hypr-GlassWindow/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
